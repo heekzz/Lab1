2f1b22d849286a78eeed3f7b9e97f4098e35de9e
InspectorDeno <dennisdufback@gmail.com>
2014-11-14 12:17:05 +0100

    java3-meck

--- app/src/main/java/freha_tddd13/lab1/MainActivity_3.java
+++ app/src/main/java/freha_tddd13/lab1/MainActivity_3.java
@@ -1,9 +1,16 @@
 package freha_tddd13.lab1;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class MainActivity_3 extends Activity {
@@ -11,7 +18,35 @@ public class MainActivity_3 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_3);
+       // setContentView(R.layout.activity_main_3);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+
+        TextView text1 = new TextView(this);
+        text1.setText("Hur trivs du på LIU");
+
+        //RadioGroup group1 = new RadioGroup(this);
+
+        TextView text2 = new TextView(this);
+        text2.setText("Läser du på LITH");
+
+        TextView text3 = new TextView(this);
+        text3.setText("Är detta LIUs logotyp");
+
+        Button button = new Button(this);
+        button.setText("Skicka in");
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        text1.setLayoutParams(lparams);
+        layout.addView(text1);
+
     }
 
 
